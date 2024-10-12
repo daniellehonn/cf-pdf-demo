11599dc48fcaa9b62d8fdc5d6af716a05ed27025
danielle hon <daniellehon@danielles-Air.lan>
2024-10-12 14:40:00 -0700

    changed to stop errors regarding youtube playlist urls

--- utils/youtube.py
+++ utils/youtube.py
@@ -3,6 +3,7 @@ from langchain_community.document_loaders.youtube import TranscriptFormat
 
 # Function to turn youtube into Langchain Documents
 def youtube_to_docs(youtube_url):
+    youtube_url = youtube_url.split('&')[0]
     transcripts = YoutubeLoader.from_youtube_url(
         youtube_url, 
         add_video_info=True,
